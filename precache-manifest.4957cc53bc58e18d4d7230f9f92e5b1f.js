e20e2926d1be637559a9a0b91cfbb5ef1d9fc027
Kate Land <kateqland@gmail.com>
2020-07-19 21:53:55 -0400

    Updates

---
+++ precache-manifest.4957cc53bc58e18d4d7230f9f92e5b1f.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b998ea84ef609489d0092eba307982f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94dc027b7c9d0387b696",
+    "url": "/static/css/main.b29c76ec.chunk.css"
+  },
+  {
+    "revision": "525ea1f0e9ff186b98a7",
+    "url": "/static/js/2.c25b1833.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c25b1833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dc027b7c9d0387b696",
+    "url": "/static/js/main.29726980.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "00b59db6e991e06c304298028921244f",
+    "url": "/static/media/Michigan_bench.00b59db6.jpg"
+  },
+  {
+    "revision": "31077a66c25bb0e5ccdf6d476e3bb1c8",
+    "url": "/static/media/USC_trees.31077a66.jpg"
+  },
+  {
+    "revision": "0e2eea097bb23bbde4f4309090b7ef44",
+    "url": "/static/media/art.0e2eea09.png"
+  },
+  {
+    "revision": "5af9634df867fe1acc5d17f85c3be7c5",
+    "url": "/static/media/beach_walk.5af9634d.jpg"
+  },
+  {
+    "revision": "bc9c0965aa4ed8e9f7d75295077377b1",
+    "url": "/static/media/blue_lake.bc9c0965.jpg"
+  },
+  {
+    "revision": "c219bab7d653adff741002b1331e1067",
+    "url": "/static/media/campfire.c219bab7.jpg"
+  },
+  {
+    "revision": "f4394e049064cb50557b0dcec7e4bdff",
+    "url": "/static/media/cat_walking.f4394e04.png"
+  },
+  {
+    "revision": "7827581e0da06907f75bfb466efefd16",
+    "url": "/static/media/china_bench.7827581e.jpg"
+  },
+  {
+    "revision": "ffcdc87e769a908e5e566ff146f2f64e",
+    "url": "/static/media/crystal_blue.ffcdc87e.jpg"
+  },
+  {
+    "revision": "946f2acc26b2e4fcf2c915e1e9321a04",
+    "url": "/static/media/duck.946f2acc.jpg"
+  },
+  {
+    "revision": "966b9f42026b99b681f799cf0ccf73ad",
+    "url": "/static/media/golden_gate.966b9f42.jpg"
+  },
+  {
+    "revision": "2c2cffaf996a22c81ab30708099da5ab",
+    "url": "/static/media/golden_gate_side.2c2cffaf.jpg"
+  },
+  {
+    "revision": "ec81f8abcd7c863d3af30c5f315ce597",
+    "url": "/static/media/hello_world.ec81f8ab.png"
+  },
+  {
+    "revision": "e49ea7c0780a963108cdf4a34e1e6676",
+    "url": "/static/media/kitty_blep.e49ea7c0.jpg"
+  },
+  {
+    "revision": "002f3ec564235f387177b322882cd4dc",
+    "url": "/static/media/lake_sunset.002f3ec5.jpg"
+  },
+  {
+    "revision": "8202e49b9086559806fdf2501e4f8d09",
+    "url": "/static/media/photography.8202e49b.jpg"
+  },
+  {
+    "revision": "91f60e0a3b2dbeb4bae37b4efa474677",
+    "url": "/static/media/pink_flower.91f60e0a.jpg"
+  },
+  {
+    "revision": "11c953ff276372ec61b7871aea0fcc0f",
+    "url": "/static/media/pothole(1).11c953ff.png"
+  },
+  {
+    "revision": "f585d616b349ff1f271976762c8a53ee",
+    "url": "/static/media/pothole(2).f585d616.png"
+  },
+  {
+    "revision": "ec73744c6f67382081bb7e9a77a81e9d",
+    "url": "/static/media/pothole(3).ec73744c.png"
+  },
+  {
+    "revision": "df96303d335cf6364c1c2908fc3da3ba",
+    "url": "/static/media/pothole(4).df96303d.png"
+  },
+  {
+    "revision": "86d4c9a589c7f0e9329ffee971e5e5f5",
+    "url": "/static/media/pothole(5).86d4c9a5.png"
+  },
+  {
+    "revision": "c44883822ae0bff42bad62b279f47521",
+    "url": "/static/media/pothole(6).c4488382.png"
+  },
+  {
+    "revision": "efc90803c9aabed50049dc2fc7f7af63",
+    "url": "/static/media/rocky_bridge.efc90803.jpg"
+  },
+  {
+    "revision": "64f872001a87b84bee522e4d0cd50fc2",
+    "url": "/static/media/rose.64f87200.jpg"
+  },
+  {
+    "revision": "f7c9851e5620e0e101ebbf5b2aafebee",
+    "url": "/static/media/rose_light.f7c9851e.png"
+  },
+  {
+    "revision": "87f1402ab4a858318d69345b46219446",
+    "url": "/static/media/sanDiego_beach.87f1402a.jpg"
+  },
+  {
+    "revision": "da758106a3ab657ec7f628db62aab621",
+    "url": "/static/media/sleeping_seal.da758106.jpg"
+  },
+  {
+    "revision": "03280211ce0622888f3bc2deaa0c4541",
+    "url": "/static/media/sleeping_seal_sunset.03280211.jpg"
+  },
+  {
+    "revision": "67d0bbef920cb116a34e9651d014f06e",
+    "url": "/static/media/stormy_blue.67d0bbef.jpg"
+  },
+  {
+    "revision": "30f186573fc7aa99f28e8eceb0dd6522",
+    "url": "/static/media/woods.30f18657.jpg"
+  },
+  {
+    "revision": "9ad385caafdecfabfd26c95deba2bdfd",
+    "url": "/static/media/yellow_bloom.9ad385ca.jpg"
+  }
+]);
