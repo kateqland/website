ba21e264d43852ce5b9efb8a10a6bfdb51d8efc4
Kate Land <kateqland@gmail.com>
2020-07-19 22:26:25 -0400

    fixed photo size on mobile

--- src/components/art.js
+++ src/components/art.js
@@ -13,6 +13,10 @@ function research() {
         <div className="art_body">
             <p className="title">Art</p>
         </div>
+
+        <div className = "art_container">
+          <h1>Coming Soon</h1>
+        </div>
       </div>
     );
 }
