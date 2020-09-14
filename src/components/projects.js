35e3efa8e1e70456eb359045b7b1724c7cf5167f
Kate Land <kateqland@gmail.com>
2020-09-14 15:02:23 -0400

    added nova project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../styles/projects.css';
 import '../styles/general.css';
-import { HashLink as Link } from 'react-router-hash-link';
-// pothole
+// images
 import pothole from '../images/projects/pixil-frame-0.png';
-// easygov
 import easygov from '../images/projects/easygov.png';
+import nova from '../images/projects/holding_hands.png';
 
 function Projects() {
     return (
@@ -21,7 +20,7 @@ function Projects() {
             <div className="project" style={{marginTop:'0px'}}>
                 <div className="project_card">
                     <p className="project_name">Holed Up</p>
-                    <img src={pothole} alt="Patch the pothole (logo)"></img>
+                    <img src={pothole} alt="Patch the Pothole (logo)"></img>
                 </div>
                 <div className="project_description">
                     <p>
@@ -40,10 +39,24 @@ function Projects() {
                 </div>
                 <div className="project_description">
                     <p>
-                    EasyGov is an online service for immigrants and non-native English speakers. 
-                    We provide translated government forms that users can fill out and have their responses downloaded in the official English version of the form. 
-                    I built the main pages of the website using React, GatsbyJS, and GraphQL. I also designed and built the form components in Storybook using React. 
-                    Additionally, I implemented a backend function that automatically sends an email response to users. This was done using NodeJS and RESTful API. 
+                        EasyGov is an online service for immigrants and non-native English speakers. 
+                        We provide translated government forms that users can fill out and have their responses downloaded in the official English version of the form. 
+                        I built the main pages of the website using React, GatsbyJS, and GraphQL. I also designed and built the form components in Storybook using React. 
+                        Additionally, I implemented a backend function that automatically sends an email response to users. This was done using NodeJS and RESTful API. 
+                    </p>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project_card">
+                    <p className="project_name">Nova</p>
+                    <img src={nova} alt="Nova (logo)" style={{marginBottom: '16px'}}></img>
+                </div>
+                <div className="project_description">
+                    <p>
+                        Nova is an online service for univserity students to report accounts of sexual harassment. All reports will be automatically 
+                        sent to be filed. Users can input their unique number to track their report progress. We store the information through 
+                        Django's database. I built the entire frontend with JavaScript, HTML, and CSS using React.
                     </p>
                 </div>
             </div>
--- src/components/projects_photos.js
+++ src/components/projects_photos.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../styles/projects.css';
 import '../styles/general.css';
-import { HashLink as Link } from 'react-router-hash-link';
 // pothole
 import pothole_1 from '../images/projects/pothole(1).png';
 import pothole_2 from '../images/projects/pothole(2).png';
@@ -9,6 +8,14 @@ import pothole_3 from '../images/projects/pothole(3).png';
 import pothole_4 from '../images/projects/pothole(4).png';
 import pothole_5 from '../images/projects/pothole(5).png';
 import pothole_6 from '../images/projects/pothole(6).png';
+// pothole
+import nova_1 from '../images/projects/nova1.png';
+import nova_2 from '../images/projects/nova2.png';
+import nova_3 from '../images/projects/nova3.png';
+import nova_4 from '../images/projects/nova4.png';
+import nova_5 from '../images/projects/nova5.png';
+import nova_6 from '../images/projects/nova6.png';
+import nova_7 from '../images/projects/nova7.png';
 
 function Projects_Photos() {
     return (
@@ -31,6 +38,25 @@ function Projects_Photos() {
                     <img src={pothole_4} alt="rating screen" className="p_image"></img>
                     <img src={pothole_5} alt="thank you screen" className="p_image"></img>
                     <img src={pothole_6} alt="already reported screen" className="p_image"></img>
+                </div>
+            </div>
+
+            <div className="project_column" style={{marginTop:'0px'}}>
+                <div className="project_card" style={{border: 'none', borderBottom: '2px solid #A2D6DB'}}>
+                    <p className="project_name" style={{marginBottom: '8px'}}>Nova</p>
+                    <a className="link" href= 'https://youtu.be/srxU6hzMhKo' target="_blank" 
+                    style={{color: 'black', textDecoration: 'underline', marginBottom: '8px'}}>Demo</a>
+                </div>
+                <div className="project_images">
+                    <div className="grid_2">
+                        <div className="grid-item"><img src={nova_1} alt="home screen" className="p_image_horiz"></img></div>
+                        <div className="grid-item"><img src={nova_2} alt="report screen" className="p_image_horiz"></img></div>
+                        <div className="grid-item"><img src={nova_3} alt="email warning screen" className="p_image_horiz"></img></div>
+                        <div className="grid-item"><img src={nova_4} alt="submission screen" className="p_image_horiz"></img></div>
+                        <div className="grid-item"><img src={nova_5} alt="tracking screen" className="p_image_horiz"></img></div>
+                        <div className="grid-item"><img src={nova_6} alt="report status screen" className="p_image_horiz"></img></div>
+                        <div className="grid-item"><img src={nova_7} alt="report status screen 2" className="p_image_horiz"></img></div>
+                    </div>
                 </div>
             </div>
         </div>
